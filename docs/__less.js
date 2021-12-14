41e68e4c349c5595d85001c99357f1b872770cec
dilrong <dilrong@dilrong.com>
2021-12-14 21:42:28 +0900

    styles: 모바일 지갑연결 스타일링

--- docs/__less.js
+++ docs/__less.js
@@ -7,43 +7,26 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./docs/style/main.less":
 /*!******************************!*\
   !*** ./docs/style/main.less ***!
   \******************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (() => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./docs/style/main.less?");
+eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nModuleBuildError: Module build failed (from ./node_modules/less-loader/dist/cjs.js):\\n\\n\\n    border-radius   : 4px;\\n    background-color: @shadow;\\n                    ^\\nVariable @shadow is undefined\\n      Error in /Users/hakseonglee/Desktop/dsc/mix-app/docs/style/component/connectwalletpopup.less (line 7, column 22)\\n    at processResult (/Users/hakseonglee/Desktop/dsc/mix-app/node_modules/webpack/lib/NormalModule.js:751:19)\\n    at /Users/hakseonglee/Desktop/dsc/mix-app/node_modules/webpack/lib/NormalModule.js:853:5\\n    at /Users/hakseonglee/Desktop/dsc/mix-app/node_modules/loader-runner/lib/LoaderRunner.js:399:11\\n    at /Users/hakseonglee/Desktop/dsc/mix-app/node_modules/loader-runner/lib/LoaderRunner.js:251:18\\n    at context.callback (/Users/hakseonglee/Desktop/dsc/mix-app/node_modules/loader-runner/lib/LoaderRunner.js:124:13)\\n    at Object.lessLoader (/Users/hakseonglee/Desktop/dsc/mix-app/node_modules/less-loader/dist/index.js:52:5)\");\n\n//# sourceURL=webpack:///./docs/style/main.less?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
-/******/ 	// The require scope
-/******/ 	var __webpack_require__ = {};
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./docs/style/main.less"](0, __webpack_exports__, __webpack_require__);
+/******/ 	__webpack_modules__["./docs/style/main.less"]();
 /******/ 	
 /******/ })()
 ;
