dae7562c24703feb4a1379e4119b139879dc7de4
dilrong <dilrong@dilrong.com>
2021-12-15 23:22:44 +0900

    fix: gitignore 오류 수정

--- node_modules/@hanul/debouncer/Debouncer.d.ts
+++
@@ -1,8 +0,0 @@
-export default class Debouncer {
-    private debounceTime;
-    private work;
-    private debounceTimeout;
-    constructor(debounceTime: number, work: () => void);
-    run(): void;
-}
-//# sourceMappingURL=Debouncer.d.ts.map
