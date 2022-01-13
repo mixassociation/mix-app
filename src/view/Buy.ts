1b6709938d726a240a17286b5e9c77b20f769013
dilrong <dilrong@dilrong.com>
2022-01-14 00:10:40 +0900

    feat: MIX 지갑 추가 외
    - 메인 line-height 조정
    - 가격 클릭 > 차트 이동
    - 믹스 소각풀 > 믹스 소각 이동

--- src/view/Buy.ts
+++ src/view/Buy.ts
@@ -1,5 +1,7 @@
 import { DomNode, el } from "@hanul/skynode";
 import { View, ViewParams } from "skyrouter";
+import MixContract from "../contracts/mix/MixContract";
+import Wallet from "../klaytn/Wallet";
 import Layout from "./Layout";
 
 export default class Buy implements View {
@@ -16,9 +18,19 @@ export default class Buy implements View {
                     el("h4", "KLAY Swap"),
                 ),
                 el(".paragraph", "MIX는 국내 최대 DEFI 서비스인 KLAYSWAP에서 구매하실 수 있습니다."),
-                el("a", "MIX 구매하기 >", {
-                    href: "https://klayswap.com/exchange/swap?input=0x0000000000000000000000000000000000000000&output=0xdd483a970a7a7fef2b223c3510fac852799a88bf"
-                })
+                el(".link-container",
+                    el("a", "MIX 구매하기 >", {
+                        href: "https://klayswap.com/exchange/swap?input=0x0000000000000000000000000000000000000000&output=0xdd483a970a7a7fef2b223c3510fac852799a88bf"
+                    }),
+                    el("a.add-mix", "MIX 지갑에 추가하기 >", {
+                        click: () => Wallet.addToken(
+                            MixContract.address,
+                            "MIX",
+                            8,
+                            "https://avatars.githubusercontent.com/u/94335451?s=200&v=4"
+                        ),
+                    })
+                )
             )
         );
     }
--- src/view/Home.ts
+++ src/view/Home.ts
@@ -25,25 +25,29 @@ export default class Home implements View {
                 ),
                 el("img.mix", { src: "/images/logo/mix.svg", height: "72px" }),
                 el("h1", "NFT 프로젝트 허브를 위한 토큰"),
-                el(".button-container", el("button.buy-mix-btn", "믹스 구매하기", {
-                    click: () => {
-                        ViewUtil.go("/buy");
-                    }
-                }), el("button.white-paper-btn", "백서 보기", {
-                    click: () => {
-                        window.open("https://medium.com/dogesoundclub/dsc-mix-nft-%ED%97%88%EB%B8%8C%EB%A5%BC-%EC%9C%84%ED%95%9C-%ED%86%A0%ED%81%B0-3299dd3a8d1d");
-                    }
-                })),
+                el(".button-container",
+                    el("button.buy-mix-btn", "믹스 구매하기", {
+                        click: () => {
+                            ViewUtil.go("/buy");
+                        }
+                    }),
+                    el("button.white-paper-btn", "백서 보기", {
+                        click: () => {
+                            window.open("https://medium.com/dogesoundclub/dsc-mix-nft-%ED%97%88%EB%B8%8C%EB%A5%BC-%EC%9C%84%ED%95%9C-%ED%86%A0%ED%81%B0-3299dd3a8d1d");
+                        }
+                    }),
+                ),
                 el(".paragraph", "MIX는 NFT 프로젝트들의 허브를 위한 토큰입니다.\nDSC 사이트의 전 범위에서 사용되며, Klayswap에서 유동성 공급 및 거래에 사용될 예정입니다.\n또한 MIX를 활용한 기능을 추가하기로 약속한 파트너 프로젝트의 서비스에서도 사용될 예정입니다."),
                 el(".overview-container",
                     el("section",
-                        el(".price-container", el(".paragraph", "1믹스 가격"),
+                        el("a.price-container", { href: "https://dexata.kr/?tokenA=0xdd483a970a7a7fef2b223c3510fac852799a88bf&tokenB=", target: "_blank" },
+                            el(".paragraph", "1믹스 가격"),
                             this.priceDisplay = el("h4", new Loading()),
                             el("h4", "원")),
                         el(".price-container", el(".paragraph", "믹스 발행량"),
                             this.poolDisplay = el("h4", new Loading()),
                             el("h4", "MIX")),
-                        el(".price-container",
+                        el("a.price-container", { click: () => {ViewUtil.go("burn")} },
                             el(".paragraph", "믹스 소각풀"),
                             this.burnableDisplay = el("h4", new Loading()),
                             el("h4", "MIX")),
